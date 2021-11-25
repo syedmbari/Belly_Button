10de21445ef16a34bf4fb131dbbe3815714b17ab
syedmbari <55304055+syedmbari@users.noreply.github.com>
2021-11-24 23:31:49 -0500

    Add files via upload

---
+++ Belly_Button/plots.js
@@ -0,0 +1,134 @@
+//Build function to read json file using d3
+function buildMetaData(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    console.log(metadata);
+
+  // Filter the data
+  var buildingArray = metadata.filter(sampleObj => sampleObj.id == sample);
+  var result = buildingArray[0];
+  // Use d3 to select the required panel
+  var panelData = d3.select("#sample-metadata");
+
+  // Clear the existing data in the html
+  panelData.html("");
+  var wfreq = result.wfreq
+
+  var data = [
+    {
+      domain: { x: [0, 1], y: [0, 1] },
+      value: wfreq,
+      title: { text: "Belly Button Washing Frequency\nScrubs per Week " },
+      subtitle:"Scrubs per Week",
+      
+      type: "indicator",
+      mode: "gauge+number+delta",
+      gauge: {
+        axis: { range: [null, 10] },
+        bar: { color: "yellow" },
+        steps: [
+          { range: [0, 2], color: "#FF4500" },
+          { range: [2, 4], color: "#FF6347" },
+          { range: [4, 6], color: "#FF7F50" },
+          { range: [6, 8], color: "#FF8C00" },
+          { range: [8, 10], color: "#FFA500" }
+        ],
+       
+      }
+    }
+  ];
+  
+  var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+  Plotly.newPlot('gauge', data, layout);
+
+  // Use `Object.entries` to add each key and value pair to the panelData
+  Object.entries(result).forEach(([key, value]) => {
+    panelData.append("h6").text(`${key.toUpperCase()}: ${value}`);
+  });
+});
+}
+
+function buildCharts(sample) {
+  d3.json("samples.json").then((data) => {
+    var sampleData = data.samples;
+    var buildingArray = sampleData.filter(sampleObj => sampleObj.id == sample);
+    var result = buildingArray[0];
+
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+
+    // Build a Bubble Chart
+  var bubbleChart = {
+      title: "Bacteria Cultures Per Sample",
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+    };
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+    ];
+
+    Plotly.newPlot("bubble", bubbleData, bubbleChart);
+    
+    //Create a horizontal bar chart
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+    var barData = [
+      {
+        y: yticks,
+        x: sample_values.slice(0, 10).reverse(),
+        text: otu_labels.slice(0, 10).reverse(),
+        type: "bar",
+        orientation: "h",
+      }
+    ];
+
+    var chartLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
+    };
+
+    Plotly.newPlot("bar", barData, chartLayout);
+  });
+};
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selectDropdown = d3.select("#selDataset");
+
+  // Populate the select options by using the list of sample names
+  d3.json("samples.json").then((data) => {
+    var name = data.names;
+
+    name.forEach((sample) => {
+      selectDropdown
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    })
+
+    // Use the sample data from the list to build the plots
+    var sampleData = name[0];
+    buildCharts(sampleData);
+    buildMetaData(sampleData);
+  });
+};
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetaData(newSample);
+};
+
+
+// Initialize the dashboard
+init()
